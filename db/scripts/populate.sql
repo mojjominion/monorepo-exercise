f0e4696d0697228c0497b1ea7564f1653807459d
Mohan <mohan@stackbox.xyz>
2023-12-19 15:55:38 +0530

    build: :hammer: add data population scripts

---
+++ db/scripts/populate.sql
@@ -0,0 +1,18 @@
+-- POPULATE users table
+insert into users(name) values('person1');
+insert into users(name) values('person2');
+insert into users(name) values('person3');
+insert into users(name) values('person4');
+insert into users(name) values('person5');
+insert into users(name) values('person6');
+insert into users(name) values('person7');
+insert into users(name) values('person8');
+
+-- POPULATE fruits table
+insert into fruits(name, addedBy) values('apple', 1);
+insert into fruits(name, addedBy) values('banana', 2);
+insert into fruits(name, addedBy) values('grapes', 3);
+insert into fruits(name, addedBy) values('orange', 4);
+insert into fruits(name, addedBy) values('peach', 5);
+insert into fruits(name, addedBy) values('mango', 4);
+insert into fruits(name, addedBy) values('papaya', 5);
