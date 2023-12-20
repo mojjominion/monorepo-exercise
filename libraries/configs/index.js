c720ce35315df94db18b946af5b3822a2fd18c02
Mohan <mohan@stackbox.xyz>
2023-12-20 23:55:35 +0530

    feat: :sparkles: improve chdir logic

--- libraries/configs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIG = void 0;
-process.chdir("../../");
-require("dotenv").config();
-exports.CONFIG = {
-    DB_URL: process.env.DATABASE_URL,
-};
-console.log("DEBUGPRINT[1]: index.ts:3: CONFIG=", exports.CONFIG);
--- libraries/configs/index.ts
+++ libraries/configs/index.ts
@@ -1,5 +1,8 @@
-process.chdir("../../");
-require("dotenv").config();
+import { config } from "dotenv";
+import path from "path";
+
+process.chdir(path.join(__dirname, ".."));
+config();
 
 export const CONFIG = {
   DB_URL: process.env.DATABASE_URL,
